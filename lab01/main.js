710b56c424661036bcf1f3e11ef0d4a96eb14bd9
jinmin.kim <bmpr14@gmail.com>
2023-12-02 17:02:14 +0900

    feat: Add various lighting options, update yarn

--- lab01/main.js
+++ lab01/main.js
@@ -17,14 +17,22 @@ async function main() {
 
   const scene = new THREE.Scene();
 
-  addLights(scene);
   addFloor(scene);
+  addLights(scene, "directional");
 
-  addShapes(scene);
-  addParticles(scene);
-  addDoubleSidedMesh(scene);
-  addTorusKnots(scene);
-  addTextureMesh(scene, await loadTexture("/threejs.webp"));
+  // addShapes(scene);
+  // addParticles(scene);
+  // addDoubleSidedMesh(scene);
+  // addTorusKnots(scene);
+  // addTextureMesh(scene, await loadTexture("/threejs.webp"));
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.castShadow = true;
+  mesh.receiveShadow = true;
+  mesh.position.y = 0.5;
+  scene.add(mesh);
 
   const orbitControls = new OrbitControls(camera, renderer.domElement);
   orbitControls.update();
@@ -50,20 +58,72 @@ function createCamera() {
   return camera;
 }
 
-function addLights(scene) {
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
-  directionalLight.castShadow = true;
-  directionalLight.position.set(3, 4, 5);
-  directionalLight.lookAt(0, 0, 0);
-  scene.add(directionalLight);
+function addLights(scene, lightType) {
+  switch (lightType) {
+    case "directional":
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+      directionalLight.castShadow = true;
+      directionalLight.position.set(3, 4, 5);
+      directionalLight.lookAt(0, 0, 0);
+      scene.add(directionalLight);
+      // helper
+      const directionalLightHelper = new THREE.DirectionalLightHelper(
+        directionalLight,
+        1
+      );
+      scene.add(directionalLightHelper);
+      break;
+
+    case "hemisphere":
+      const hemisphereLight = new THREE.HemisphereLight(0xb4a912, 0x12f34f, 5);
+      hemisphereLight.position.set(0, 1, 0);
+      hemisphereLight.lookAt(0, 0, 0);
+      scene.add(hemisphereLight);
+      break;
+
+    case "point":
+      const pointLight = new THREE.PointLight(0xffffff, 5, 5, 4);
+      pointLight.castShadow = true;
+      pointLight.position.set(1, 1, 1);
+      scene.add(pointLight);
+      break;
+
+    case "rectArea":
+      const rectAreaLight = new THREE.RectAreaLight(0xffffff, 5, 2, 2);
+      rectAreaLight.castShadow = true;
+      rectAreaLight.position.set(1, 1, 2);
+      scene.add(rectAreaLight);
+      break;
+
+    case "spot":
+      const targetObj = new THREE.Object3D();
+      scene.add(targetObj);
+      const spotLight = new THREE.SpotLight(
+        0xffffff,
+        10,
+        100,
+        Math.PI / 4,
+        1,
+        1
+      );
+      spotLight.castShadow = true;
+      spotLight.position.set(0, 3, 0);
+      spotLight.target = targetObj;
+      spotLight.target.position.set(1, 0, 2);
+      scene.add(spotLight);
+      break;
+  }
 }
 
 function addFloor(scene) {
   const geometry = new THREE.PlaneGeometry(20, 20);
-  const material = new THREE.MeshStandardMaterial({ color: 0xbbbbbb });
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xbbbbbb,
+  });
   const floor = new THREE.Mesh(geometry, material);
   floor.rotation.x = -Math.PI / 2;
   floor.receiveShadow = true;
+  floor.castShadow = true;
   scene.add(floor);
 }
 
